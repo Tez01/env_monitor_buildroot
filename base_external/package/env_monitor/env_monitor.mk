1bfd196628722556c5b405b43b7bb1c0f4a69fb6
Tej <tejpartap02@gmail.com>
2026-08-03 13:10:00 -0400

    Save Raspberry Pi Buildroot configuration with wifi support

--- base_external/package/env_monitor/env_monitor.mk
+++ base_external/package/env_monitor/env_monitor.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-ENV_MONITOR_VERSION = 88b006c697f0c618683a6087484b2b225d20ee41
+ENV_MONITOR_VERSION = e9ff011f80a01f5191a09ad2b71cfaa27b89187c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,13 +14,12 @@ ENV_MONITOR_SITE_METHOD = git
 ENV_MONITOR_GIT_SUBMODULES = YES
 
 define ENV_MONITOR_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/src all
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/src all
 endef
 
 define ENV_MONITOR_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/env_monitor $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/src/env_monitor-start-stop $(TARGET_DIR)/etc/init.d/S97env_monitor
+	$(INSTALL) -D -m 0755 $(@D)/src/env_monitor $(TARGET_DIR)/usr/bin/env_monitor
+	$(INSTALL) -D -m 0755 $(@D)/src/env_monitor-start-stop $(TARGET_DIR)/etc/init.d/S98env_monitor
 endef
 
 $(eval $(generic-package))
-
