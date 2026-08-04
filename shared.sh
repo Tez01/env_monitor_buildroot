224a05f8e2b008ae1d7ebfef6625a9efba81c6e1
Tej <tejpartap02@gmail.com>
2026-08-04 19:52:44 -0400

    Modify save script to save rpi related info

--- shared.sh
+++ shared.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# Shared definitions for buildroot scripts
+# Shared definitions for Buildroot scripts
 
-# The defconfig from the buildroot directory we use for qemu builds
-QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
-# The place we store customizations to the qemu configuration
-MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
-# The defconfig from the buildroot directory we use for the project
-AESD_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+# Standard Raspberry Pi 4 64-bit configuration supplied by Buildroot
+RPI_DEFAULT_DEFCONFIG=configs/raspberrypi4_64_defconfig
+
+# Project-specific Raspberry Pi configuration
+RPI_MODIFIED_DEFCONFIG=base_external/configs/raspberrypi4_64_env_monitor_defconfig
+
+AESD_DEFAULT_DEFCONFIG=${RPI_DEFAULT_DEFCONFIG}
+AESD_MODIFIED_DEFCONFIG=${RPI_MODIFIED_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
