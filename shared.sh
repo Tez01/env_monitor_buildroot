d072257c4c63867c8ed617200be2f8d469833fe1
Tej <tejpartap02@gmail.com>
2026-08-04 19:54:46 -0400

    Allow shared.sh to run both for qemu and rpi

--- shared.sh
+++ shared.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Shared definitions for Buildroot scripts
 
-# Standard Raspberry Pi 4 64-bit configuration supplied by Buildroot
-RPI_DEFAULT_DEFCONFIG=configs/raspberrypi4_64_defconfig
+# QEMU configurations
+QEMU_DEFAULT_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
+QEMU_MODIFIED_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
 
-# Project-specific Raspberry Pi configuration
+# Raspberry Pi 4 configurations
+RPI_DEFAULT_DEFCONFIG=configs/raspberrypi4_64_defconfig
 RPI_MODIFIED_DEFCONFIG=base_external/configs/raspberrypi4_64_env_monitor_defconfig
 
+# Select the project configuration used by helper scripts
 AESD_DEFAULT_DEFCONFIG=${RPI_DEFAULT_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG=${RPI_MODIFIED_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
